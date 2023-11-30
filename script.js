2fad3ef9b15729f5a82ce54b627c8bf23fb2fa00
Mr_RajaG <rajataha062@gmail.com>
2023-11-30 22:53:30 +0500

    version 0.3

--- script.js
+++ script.js
@@ -1,32 +1,43 @@
 let userTypeElement = document.getElementById("userType");
-let adminOption = document.querySelector(".adminOption");
+let adminOption1 = document.getElementById("project-option");
+let adminOption2 = document.getElementById("conference-option");
+let loginForm = document.getElementById("login-form");
+
+if (userTypeElement.innerHTML.toLowerCase() === "user") {
+  adminOption1.style.visibility = "hidden";
+  adminOption2.style.visibility = "hidden";
+} else if (userTypeElement.innerHTML.toLowerCase() === "admin") {
+  loginForm.style.visibility = "hidden";
+  adminOption1.style.visibility = "visible";
+  adminOption2.style.visibility = "visible";
+} else {
+  console.log("Invalid User");
+}
 
 function switchUser() {
-  let loginForm = document.getElementById("login-form");
-
   if (userTypeElement.innerHTML.toLowerCase() === "user") {
     loginForm.style.visibility = "visible";
-    adminOption.style.visibility = "hidden";
   } else if (userTypeElement.innerHTML.toLowerCase() === "admin") {
-    loginForm.style.visibility = "hidden";
-    adminOption.style.visibility = "visible";
+    adminOption1.style.visibility = "hidden";
+    adminOption2.style.visibility = "hidden";
     userTypeElement.innerHTML = "User";
   } else {
     console.log("Invalid User");
   }
 }
 
-function loginSubmit(event) {
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
+function loginSubmit() {
+  var username = document.getElementById("username");
+  var password = document.getElementById("password");
 
-  if (username === "taha" && password === "taha") {
+  if (username.value === "taha" && password.value === "taha") {
+    loginForm.style.visibility = "hidden";
+    username.value = "";
+    password.value = "";
     userTypeElement.innerHTML = "Admin";
-    adminOption.style.visibility = "visible";
+    adminOption1.style.visibility = "visible";
+    adminOption2.style.visibility = "visible";
   } else {
     alert("Incorrect username or password. Please try again.");
   }
-
-  event.preventDefault();
-  return false;
 }
